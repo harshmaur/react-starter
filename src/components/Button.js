a0314e10b23ab688f4d23ea686f96eb06d64c5af
Harsh Maur <harshmaur@gmail.com>
2017-05-12 17:29:01 +0530

    added flowconfig

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledButton = styled.button`
   height: 30px;
   border: none;
   border-radius: 5px;
   background-color: skyblue;
-`
+`;
 
-const Button = ({text}) => <StyledButton type="button">{text}</StyledButton>
+const Button = ({ text }): Object => (
+  <StyledButton type="button">{text}</StyledButton>
+);
 
 export { Button };
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Link from 'react-router-dom/Link'
-import { Button } from '../components';
+import React from "react";
+import Link from "react-router-dom/Link";
+import { Button } from "../components";
 
 const NoMatch = () => (
   <div>
     Page Not Found <br />
     <Link to="/"><Button text="Home Page" /></Link>
   </div>
-)
+);
 
 export { NoMatch };
